f0eab4d41f98ead30e53975e5e54089721a12595
ArthurPeruzzo <arthur.kohl@viasoft.com.br>
2023-12-13 19:20:47 -0300

    TR-1 - Testes unitários ModelMapperRecordTest

---
+++ src/test/java/com/projeto/transferencia/util/ModelMapperRecordTest.java
@@ -0,0 +1,26 @@
+package com.projeto.transferencia.util;
+
+import com.projeto.transferencia.util.modelMapper.ModelMapperRecord;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.record.RecordValueReader;
+
+public class ModelMapperRecordTest {
+
+    @Test
+    public void deveRetornarMesmaInstancia() {
+        ModelMapper primeiraInstancia = ModelMapperRecord.getModelMapperRecord();
+        ModelMapper segundaInstancia = ModelMapperRecord.getModelMapperRecord();
+
+        Assertions.assertSame(primeiraInstancia, segundaInstancia);
+    }
+
+    @Test
+    public void deveRegistrarRecordModule() {
+        ModelMapper modelMapper = ModelMapperRecord.getModelMapperRecord();
+
+        Assertions.assertTrue(modelMapper.getConfiguration().getValueReaders()
+                .stream().anyMatch(value -> value instanceof RecordValueReader));
+    }
+}
